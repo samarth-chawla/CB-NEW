a9ef66dcef6db2563ed633e009612ae7e2b59778
Jasbir94 <167897520+Jasbir94@users.noreply.github.com>
2025-04-18 11:03:08 +0530

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,19 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Events from "./pages/Events";
-import {EventsPage} from "./pages/EventsPage";
 import Hackblocks from './pages/hackblocks';
 import Carnival from './pages/carnival';
 import PixelToPitch from './pages/pixelToPitch';
 import Uxelerate from './pages/uxelerate'
 import Community from "./pages/Community";
 import Resources from "./pages/Resources";
+import NotFound from "./pages/NotFound";
+import FacultyMentor from './components/FacultyMentor'
+import ChairPerson from './components/ChairPerson'
+import Heads from './components/Heads'
+import {EventsPage} from "./pages/EventsPage";
 import Contact from "./pages/Contact";
+
 
 const queryClient = new QueryClient();
 
@@ -25,15 +30,18 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/events" element={<EventsPage />} />
+          <Route path="/events" element={<Events />} />
           <Route path='/events/hackblocks' element={<Hackblocks/>}/>
           <Route path='/events/carnival' element={<Carnival/>}/>
           <Route path='/events/p2p' element={<PixelToPitch/>}/>
           <Route path='/events/uxelerate' element={<Uxelerate/>}/>
           <Route path="/community" element={<Community />} />
+          <Route path="/community/faculty-mentor" element={<FacultyMentor />} />
+          <Route path="/community/chair-person" element={<ChairPerson />} />
+          <Route path="/community/heads" element={<Heads />} />
           <Route path="/resources" element={<Resources />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
